823cbf38d90e9f5659b1f525902256ebd6d0eaae
MR-LIMs <58262954+MR-LIMs@users.noreply.github.com>
2020-04-15 16:01:06 +0900

    Delete Homework4.py

--- Homework4.py
+++
@@ -1,8 +0,0 @@
-print("★ 구구단을 출력합니다.")
-for y in range(0, 10):
-    for x in range(2, 10):
-        if y == 0:
-            print(str(x)+'단', end='\t'*2)
-        else:
-            print(x, "X", y, "=", x*y, end='\t')
-    print("")  
